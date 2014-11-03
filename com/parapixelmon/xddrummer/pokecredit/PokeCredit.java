0dbfb4c7f4bd5f4f40f51a39ee49c663070654ef
XDdrummer <cathamster7@gmail.com>
2014-11-03 13:55:25 -0500

    Update PokeCredit.java

--- com/parapixelmon/xddrummer/pokecredit/PokeCredit.java
+++ com/parapixelmon/xddrummer/pokecredit/PokeCredit.java
@@ -1,1 +1,17 @@
-null
+package com.parapixelmon.xddrummer.pokecredit;
+
+public class PokeCredit extends JavaPlugin{
+  
+  Logger log = Bukkit.getLogger();
+  
+  @Override
+  public void onEnable(){
+    log.info("Enabling PokeCredit Plugin..");
+  }
+  
+  @Override
+  public void onDisable(){
+    log.info("Disabling PokeCredit Plugin..");
+  }
+  
+}
