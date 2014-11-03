75481ec938b2b7735f1e25665f39d1e7709c914b
XDdrummer <cathamster7@gmail.com>
2014-11-03 14:01:52 -0500

    Update PlayerJoin.java

--- com/parapixelmon/xddrummer/pokecredit/event/PlayerJoin.java
+++ com/parapixelmon/xddrummer/pokecredit/event/PlayerJoin.java
@@ -2,6 +2,17 @@ package com.parapixelmon.xddrummer.pokecredit.event;
 
 public class PlayerJoin implements Listener {
 
-  
+  PokeCredit plugin;
+
+  @EventHandler
+  public static void onPlayerJoin(PlayerJoinEvent e){
+    
+    Player p = e.getPlayer();
+    
+    if(!plugin.getConfig().contains("creditBal." + p.getUniqueId()){
+     
+    }
+    
+  }
 
 }
